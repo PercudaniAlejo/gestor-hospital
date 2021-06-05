8c988a7b9473917719d306454cb4219ff8c6da49
juani18 <juany.nunnez88@gmail.com>
2021-06-05 13:07:10 -0300

    Cree la clase Contact con sus MV(mail,numberPhone,tutorsPhone) con sus properties y constructores respectivamente y el metodo to string.

---
+++ Menu/Contact.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Menu
+{
+    class Contact
+    {
+        #region MV       
+        private string mail;
+        private int numberPhone;
+        private int tutorsPhone;
+        #endregion
+        #region Properties
+        public string Mail { get => mail; set => mail = value; }
+        public int NumberPhone { get => numberPhone; set => numberPhone = value; }
+        public int TutorsPhone { get => tutorsPhone; set => tutorsPhone = value; }
+        #endregion
+        #region Methods
+        public override string ToString()
+        {
+            return mail + "" + numberPhone + "" + tutorsPhone;
+        }
+
+        #endregion
+        #region Builders
+        public Contact()
+        {
+            this.mail = "";
+            this.numberPhone = 0;
+            this.tutorsPhone = 0;
+        }
+        public Contact(string mail, int numberPhone, int tutorsPhone)
+        {
+            this.mail = mail;
+            this.numberPhone = numberPhone;
+            this.tutorsPhone = tutorsPhone;
+        }
+        #endregion
+    }
+}
