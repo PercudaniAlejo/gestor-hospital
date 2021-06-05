a4cd6005f3dab80e471f338f7bcdf69ed6024d30
juani18 <juany.nunnez88@gmail.com>
2021-06-05 12:45:14 -0300

    Cree la clase Document dividida por regiones.Hice los MV(variable y lista),las properties de ambas,el contructor de la variable y el metodo Tostring. doctype=variable docType=Lista

---
+++ Menu/Document.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Menu
+{
+    class Document
+    {
+        #region MV
+        private string doctype;
+        private static List<Document> docType = new List<Document>();
+        #endregion
+        #region Properties
+        public string Doctype { get => doctype; set => doctype = value; }
+        public static List<Document> DocType { get => docType; set => docType = value; }
+        #endregion
+        #region Methods
+        public override string ToString()
+        {
+            return doctype;
+        }
+        #endregion
+        #region Builders
+        public Document()
+        {
+            doctype = "";
+        }
+        public Document(string doctype)
+        {
+            this.doctype = doctype;
+        }
+        #endregion
+    }
+}
