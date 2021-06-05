29e75c4989bbe4bbaeaaba3f90485737b8163861
MartinStagliano <martinstagliano2002@gmail.com>
2021-06-05 17:38:31 -0300

    añadi botones al form menu

--- Menu/Menu.Designer.cs
+++ Menu/Menu.Designer.cs
@@ -29,12 +29,56 @@ namespace Menu
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnPacient = new System.Windows.Forms.Button();
+            this.btnDoctor = new System.Windows.Forms.Button();
+            this.btnDocument = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnPacient
+            // 
+            this.btnPacient.Location = new System.Drawing.Point(12, 12);
+            this.btnPacient.Name = "btnPacient";
+            this.btnPacient.Size = new System.Drawing.Size(450, 23);
+            this.btnPacient.TabIndex = 0;
+            this.btnPacient.Text = "Pacient";
+            this.btnPacient.UseVisualStyleBackColor = true;
+            // 
+            // btnDoctor
+            // 
+            this.btnDoctor.Location = new System.Drawing.Point(12, 41);
+            this.btnDoctor.Name = "btnDoctor";
+            this.btnDoctor.Size = new System.Drawing.Size(450, 23);
+            this.btnDoctor.TabIndex = 1;
+            this.btnDoctor.Text = "Doctor";
+            this.btnDoctor.UseVisualStyleBackColor = true;
+            // 
+            // btnDocument
+            // 
+            this.btnDocument.Location = new System.Drawing.Point(12, 70);
+            this.btnDocument.Name = "btnDocument";
+            this.btnDocument.Size = new System.Drawing.Size(450, 23);
+            this.btnDocument.TabIndex = 2;
+            this.btnDocument.Text = "Document";
+            this.btnDocument.UseVisualStyleBackColor = true;
+            // 
+            // Menu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(474, 105);
+            this.Controls.Add(this.btnDocument);
+            this.Controls.Add(this.btnDoctor);
+            this.Controls.Add(this.btnPacient);
+            this.Name = "Menu";
             this.Text = "Menu";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnPacient;
+        private System.Windows.Forms.Button btnDoctor;
+        private System.Windows.Forms.Button btnDocument;
     }
 }
