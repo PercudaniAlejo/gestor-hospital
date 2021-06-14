9a92d8116bb4bfee65f9894b831490c6bc77894e
MartinStagliano <martinstagliano2002@gmail.com>
2021-06-14 15:57:07 -0300

    Cargar details

--- Menu/FormDetails.cs
+++ Menu/FormDetails.cs
@@ -20,9 +20,25 @@ namespace Menu
         }
 
         private void FormEjemplo_Load(object sender, EventArgs e)
-        {
-            lblContactoMail.Text = actual.Contact.Mail;
+        {     
             lblPS.Text = actual.ToString();
+            lblDOBs.Text = actual.DateofBirth.ToShortDateString().ToString();
+            lblGS.Text = actual.Gender.ToString();
+            lblNDs.Text = actual.DocumentNumber.ToString();
+            lblTYPE.Text = actual.Document.ToString();
+            lblBTS.Text = actual.BloodType.ToString();
+            lblMails.Text = actual.Contact.Mail.ToString();
+            lblPNS.Text = actual.Contact.NumberPhone.ToString();
+            lblTNS.Text = actual.Contact.Tutor.ToString();
+            lblTPS.Text = actual.Contact.TutorsPhone.ToString();
+            lblLS.Text=actual.Location.ToString();
+            lblNS.Text = actual.HealthInsurance.Name.ToString();
+            lblPlanS.Text = actual.HealthInsurance.Plan.ToString();
+            lblNumberS.Text = actual.HealthInsurance.Number.ToString();
+            lblDDS.Text = actual.HealthInsurance.DueDate.ToShortDateString().ToString();
+            lblTS.Text = actual.HealthInsurance.Typehi.ToString();
+            lblMails.Text = actual.Contact.Mail;
+
         }
 
        
